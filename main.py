83f6f58e8c43ea5faadbe60aed3a55ab3b8a0f6b
Tim Gjernes <tgjernes@toyopumps.com>
2022-11-29 11:16:47 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,205 @@
+import sys
+from os import path, walk
+from tkinter.filedialog import askopenfilename
+
+import matplotlib
+from PyQt6.QtWidgets import QVBoxLayout, QWidget, QHBoxLayout
+
+matplotlib.use('Qt5Agg')
+
+from PyQt6 import QtCore, QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+from TCAE_Results_UI import Ui_MainWindow
+
+
+# Class to subclass QWidget to create a figure and a navigation toolbar
+class MplWidget(QWidget):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        QWidget.__init__(self)
+        self.setLayout(QVBoxLayout())
+        self.canvas = MplCanvas(self, width, height, dpi)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.layout().addWidget(self.toolbar)
+        self.layout().addWidget(self.canvas)
+
+
+# Class to subclass FigureCanvasQTAgg to easily put it in the GUI
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        # self.axes = fig.add_subplot(111)
+        FigureCanvasQTAgg.__init__(self, fig)
+        self.setParent(parent)
+        # self.plot()
+
+        # self.fig.plot()
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        resid_fig = self.ui.resids_plot.canvas.figure
+        ax = resid_fig.add_subplot(111)
+        ax.set_xlabel('Timesteps')
+        ax.set_ylabel('Residuals')
+        ax.grid(which='major')
+        ax.grid(which='minor', color='0.9')
+        resid_fig.set_tight_layout(True)
+
+        # Load TCAE file to grab/analyze results
+        self.ui.pb_load.clicked.connect(self.load_data)
+        # sc = self.ui.mons_plot
+        # ax0 = sc.canvas.figure.add_subplot(311)
+        # ax1 = sc.canvas.figure.add_subplot(312)
+        # ax2 = sc.canvas.figure.add_subplot(313)
+        # ax0.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+
+    def load_data(self):
+        # Load TCAE file
+        fn = askopenfilename(title='Select the .tcae file for the run you want to analyze',
+                             filetypes=[('TCAE files', '*tcae')])
+        if not fn:
+            return
+
+        with open(fn, 'r') as f:
+            tcae_file = f.readlines()
+
+        # Get RPM
+        rpm = 0
+        for line in tcae_file:
+            if line:
+                if 'angularVelocity' in line:
+                    rpm = line.split('"')[1]
+                    break
+        if 'RPM' in rpm:
+            rpm = float(rpm.split('_')[0])
+
+        # Files & paths for data
+        resids_fn = path.join(path.dirname(fn), r'TCFD/logRun/all.log')  # Location of file containing residuals
+        mons_path = path.join(path.dirname(fn), r'TCFD/postProcessing/efficiency-0')  # Path where monitors are located
+        if not path.isdir(mons_path):  # Check that monitors path exists
+            print('Path does not exist:\n%s' % mons_path)
+            return
+        speedlines = next(walk(mons_path))[1]  # Determine # of separate simulations run in this path
+        mons_fn = []
+        mons_final_fn = []
+        for speedline in speedlines:
+            mons_fn.append(
+                path.join(mons_path, speedline, 'efficiency.csv'))  # List of CSV files containing all simulation data
+            mons_final_fn.append(
+                path.join(mons_path, speedline, 'efficiency-final.csv'))  # List of CSV files containing final results
+
+        # Grab/plot residual data
+        self.process_resids(resids_fn)
+
+    def process_resids(self, fn):
+        # Read residual data
+        with open(fn, 'r') as log:
+            log_data = log.readlines()
+
+        # Initialize lists
+        time = []
+        Ux = []
+        Uy = []
+        Uz = []
+        p = []
+        omega = []
+        k = []
+
+        # Grab residual data from the log
+        try:
+            for i in range(len(log_data)):
+                if 'SmoothSolver' in log_data[i]:
+                    if 'Time = ' in log_data[i - 2]:
+                        time.append(int(log_data[i - 2].split()[2]))
+                        Ux.append(float(log_data[i].split()[7].split(',')[0]))
+                        Uy.append(float(log_data[i + 1].split()[7].split(',')[0]))
+                        Uz.append(float(log_data[i + 2].split()[7].split(',')[0]))
+                        p.append(float(log_data[i + 6].split()[7].split(',')[0]))
+                        if 'Solving for omega' in log_data[i + 13]:
+                            omega_row = i + 13
+                        else:
+                            omega_row = i + 14
+                        omega.append(float(log_data[omega_row].split()[7].split(',')[0]))
+                        if 'Solving for k' in log_data[i + 14]:
+                            k_row = i + 14
+                        else:
+                            k_row = i + 15
+                        k.append(float(log_data[k_row].split()[7].split(',')[0]))
+        except:
+            return
+
+        if len(time) > len(k):
+            time = time[:len(k)]
+            Ux = Ux[:len(k)]
+            Uy = Uy[:len(k)]
+            Uz = Uz[:len(k)]
+            p = p[:len(k)]
+            omega = omega[:len(k)]
+
+        iter_true = range(1, len(time) + 1)
+
+        names = ['Ux', 'Uy', 'Uz', 'p', 'omega', 'k']
+        fig = self.ui.resids_plot.canvas.figure
+        ax = self.ui.resids_plot.canvas.figure.axes[0]
+
+        Ux_plot = ax.plot(iter_true,Ux,label=names[0])
+        Uy_plot = ax.plot(iter_true,Uy,label=names[1])
+        Uz_plot = ax.plot(iter_true,Uz,label=names[2])
+        p_plot = ax.plot(iter_true,p,label=names[3])
+        omega_plot = ax.plot(iter_true,omega,label=names[4])
+        k_plot = ax.plot(iter_true,k,label=names[5])
+        lines = [Ux_plot, Uy_plot, Uz_plot, p_plot, omega_plot, k_plot]
+        ax.set_xlim(0,iter_true[-1])
+        ax.set_yscale('log')
+        ax.set_ylim(1e-5, 1)
+        fig.legend()
+        fig.canvas.draw_idle()
+
+        annot = ax.annotate("", xy=(0, 0), xytext=(3, 3), textcoords="offset points", bbox=dict(boxstyle="round", fc="w"))
+        annot.set_visible(False)
+        fig.canvas.mpl_connect("motion_notify_event", lambda event: self.hover(event, fig))
+
+    def hover(self, event, fig):
+        ax = fig.axes[0]
+        annot = ax.texts[0]
+        vis = annot.get_visible()
+
+        if event.inaxes == ax:
+            for line in ax.lines:
+                cont, ind = line.contains(event)
+                if cont:
+                    # print(line.get_label())
+                    # self.update_annot(line, ind)
+                    x, y = line.get_data()
+                    annot.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
+                    text = line.get_label()
+                    annot.set_text(text)
+                    annot.get_bbox_patch().set_alpha(0.4)
+                    annot.set_visible(True)
+                    fig.canvas.draw_idle()
+                else:
+                    if vis:
+                        annot.set_visible(False)
+                        fig.canvas.draw_idle()
+
+    # def update_annot(self, line, ind):
+    #     x, y = line.get_data()
+    #     print(line.axis.figure)
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    win = MainWindow()
+    win.show()
+
+    sys.exit(app.exec())
